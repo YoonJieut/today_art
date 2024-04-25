08f73a820fc7eeebe428c90e51fac993388f0144
YoonJieut <qmdldos@gmail.com>
2024-04-26 02:36:12 +0900

    Feat : Response data 보내보고 띄워보기

--- today_art/today_art/urls.py
+++ today_art/today_art/urls.py
@@ -15,10 +15,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
 from testapp import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.simple_view) # domain.com/first_app
+    path('', views.simple_view), # domain.com/first_app
+    path('api/', include('testapp.urls'))
 ]
